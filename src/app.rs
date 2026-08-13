5c2519fc32bed5fdfc4b96e84a0a6792fe33dc08
SomnusCachyosHP <dev@somnus.wiki>
2026-08-13 23:29:26 +0800

    feat:初步完成前端的雏形，但是没接入后端api

--- src/app.rs
+++ src/app.rs
@@ -1,59 +1,101 @@
 use leptos::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = signal("Controlled".to_string());
-    let email = RwSignal::new("".to_string());
-    let favorite_color = RwSignal::new("red".to_string());
-    let spam_me = RwSignal::new(true);
+    let is_oxidizer = RwSignal::new(true);
+    let oxidizer_amount = RwSignal::new("".to_string());
+    let fuel_amount = RwSignal::new("".to_string());
+    let selected_oxidizer = RwSignal::new("".to_string());
+    let selected_fuel = RwSignal::new("".to_string());
 
     view! {
-        <input type="text"
-            bind:value=(name, set_name)
-        />
-        <input type="email"
-            bind:value=email
-        />
         <label>
-            "Please send me lots of spam email."
+        "燃料量:"
+        <input type="number"
+            bind:value=fuel_amount
+        />
+        </label>
+        <label>
+            "是否需要氧化剂"
             <input type="checkbox"
-                bind:checked=spam_me
+                bind:checked=is_oxidizer
             />
         </label>
         <fieldset>
-            <legend>"Favorite color"</legend>
+            <legend>"火箭选择"</legend>
             <label>
-                "Red"
+                "液氢引擎"
                 <input
                     type="radio"
                     name="color"
-                    value="red"
-                    bind:group=favorite_color
+                    value="liquid_hydrogen"
+                    bind:group=selected_fuel
                 />
             </label>
             <label>
-                "Green"
+                "蒸汽引擎"
                 <input
                     type="radio"
                     name="color"
-                    value="green"
-                    bind:group=favorite_color
+                    value="stream"
+                    bind:group=selected_fuel
                 />
             </label>
             <label>
-                "Blue"
+                "石油引擎"
                 <input
                     type="radio"
                     name="color"
-                    value="blue"
-                    bind:group=favorite_color
+                    value="petroleum"
+                    bind:group=selected_fuel
                 />
             </label>
         </fieldset>
-        <p>"Your favorite color is " {favorite_color} "."</p>
-        <p>"Name is: " {name}</p>
-        <p>"Email is: " {email}</p>
-        <Show when=move || spam_me.get()>
-            <p>"You’ll receive cool bonus content!"</p>
+        <Show when=move || is_oxidizer.get()>
+        <fieldset>
+            <legend>"氧化剂"</legend>
+            <label>
+                "液氧"
+                <input
+                    type="radio"
+                    name="color"
+                    value="liquid_oxygen"
+                    bind:group=selected_oxidizer
+                />
+            </label>
+            <label>
+                "氧石"
+                <input
+                    type="radio"
+                    name="color"
+                    value="oxidizer_stone"
+                    bind:group=selected_oxidizer
+                />
+            </label>
+            <label>
+                "肥料"
+                <input
+                    type="radio"
+                    name="color"
+                    value="fertilizer"
+                    bind:group=selected_oxidizer
+                />
+            </label>
+            <label>
+                "\n 氧化剂量："
+                <input type="number"
+                    bind:value=oxidizer_amount
+                />
+            </label>
+        </fieldset>
         </Show>
+        <p>"你选择的引擎是" {selected_fuel} "."</p>
+        <p>"燃料量是: " {fuel_amount}</p>
+        <p>"是否需要氧化剂: " {is_oxidizer}</p>
+
+        <Show when=move || is_oxidizer.get()>
+        <p>"你选择的氧化剂是" {selected_oxidizer} "."</p>
+        <p>"氧化剂量是: " {oxidizer_amount}</p>
+        </Show>
+
     }
 }
