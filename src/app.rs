13b1185b12351f3a29419d2c51504f5286bba184
SomnusCachyosHP <dev@somnus.wiki>
2026-08-14 13:22:37 +0800

    feat：尝试将数据结构传入

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,24 @@
+use crate::models::{EngineKind, RocketInput};
 use leptos::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
-    let is_oxidizer = RwSignal::new(true);
     let oxidizer_amount = RwSignal::new("".to_string());
     let fuel_amount = RwSignal::new("".to_string());
     let selected_oxidizer = RwSignal::new("".to_string());
-    let selected_fuel = RwSignal::new("".to_string());
+    let engine = RwSignal::new(EngineKind::Steam);
+    let engine_name = move || engine.get().spec().name;
+    let requires_oxidizer = move || engine.get().spec().requires_oxidizer;
+    let on_calculate = move |_| match fuel_amount.get().parse::<f32>() {
+        Ok(value) => {
+            let rocket = RocketInput {
+                engine: engine.get(),
+                fuel_amount: value,
+                oxidizer_input: None,
+            };
+            leptos::logging::log!("燃料量: {}", value);
+        }
+        Err(_) => leptos::logging::log!("燃料量输入错误"),
+    };
 
     view! {
         <label>
@@ -14,50 +27,39 @@ pub fn App() -> impl IntoView {
             bind:value=fuel_amount
         />
         </label>
-        <label>
-            "是否需要氧化剂"
-            <input type="checkbox"
-                bind:checked=is_oxidizer
-            />
-        </label>
         <fieldset>
             <legend>"火箭选择"</legend>
             <label>
                 "液氢引擎"
                 <input
                     type="radio"
-                    name="color"
-                    value="liquid_hydrogen"
-                    bind:group=selected_fuel
+                    name="engine"
+                    prop:checked=move || engine.get() == EngineKind::Hydrogen
+                    on:change=move |_|{
+                        engine.set(EngineKind::Hydrogen);
+                    }
                 />
             </label>
             <label>
                 "蒸汽引擎"
                 <input
                     type="radio"
-                    name="color"
-                    value="stream"
-                    bind:group=selected_fuel
-                />
-            </label>
-            <label>
-                "石油引擎"
-                <input
-                    type="radio"
-                    name="color"
-                    value="petroleum"
-                    bind:group=selected_fuel
+                    name="engine"
+                    prop:checked=move || engine.get() == EngineKind::Steam
+                    on:change=move |_|{
+                        engine.set(EngineKind::Steam);
+                    }
                 />
             </label>
         </fieldset>
-        <Show when=move || is_oxidizer.get()>
+        <Show when=requires_oxidizer>
         <fieldset>
             <legend>"氧化剂"</legend>
             <label>
                 "液氧"
                 <input
                     type="radio"
-                    name="color"
+                    name="oxidizer"
                     value="liquid_oxygen"
                     bind:group=selected_oxidizer
                 />
@@ -66,7 +68,7 @@ pub fn App() -> impl IntoView {
                 "氧石"
                 <input
                     type="radio"
-                    name="color"
+                    name="oxidizer"
                     value="oxidizer_stone"
                     bind:group=selected_oxidizer
                 />
@@ -75,7 +77,7 @@ pub fn App() -> impl IntoView {
                 "肥料"
                 <input
                     type="radio"
-                    name="color"
+                    name="oxidizer"
                     value="fertilizer"
                     bind:group=selected_oxidizer
                 />
@@ -88,14 +90,16 @@ pub fn App() -> impl IntoView {
             </label>
         </fieldset>
         </Show>
-        <p>"你选择的引擎是" {selected_fuel} "."</p>
+        <p>"你选择的引擎是" {engine_name} "."</p>
         <p>"燃料量是: " {fuel_amount}</p>
-        <p>"是否需要氧化剂: " {is_oxidizer}</p>
+        <p>"是否需要氧化剂: " {requires_oxidizer}</p>
 
-        <Show when=move || is_oxidizer.get()>
+        <Show when=requires_oxidizer>
         <p>"你选择的氧化剂是" {selected_oxidizer} "."</p>
         <p>"氧化剂量是: " {oxidizer_amount}</p>
         </Show>
-
+        <button on:click=on_calculate>
+            "计算"
+        </button>
     }
 }
